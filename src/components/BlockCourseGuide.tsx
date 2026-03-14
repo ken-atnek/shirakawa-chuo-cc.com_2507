052b55353db895ba06c545cdbed97d052e6c4fc3
Kenta Urakami <ken.atnek@gmail.com>
2026-03-14 12:02:39 +0900

    春期 テキスト・pdf変更

--- src/components/BlockCourseGuide.tsx
+++ src/components/BlockCourseGuide.tsx
@@ -27,7 +27,7 @@ const BlockCourseGuide = () => {
       <div className={styles.wrapImage}>
         <div className={styles.itemH3}>
           <h3>
-            <>冬期募集</>
+            <>春期募集</>
           </h3>
         </div>
         <ul className={styles.listImage}>
@@ -59,7 +59,7 @@ const BlockCourseGuide = () => {
           </li>
           <li>
             <ExternalLink
-              href="/data/pdf/251212.pdf"
+              href="/data/pdf/2026年度春期A4両面チラシ.pdf"
               aria-label="カルチャー講座"
             >
               <Image
